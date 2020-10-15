ceb5e3d1c3019426cc290d1e81141c96378755b2
Muhammed <nsaryyev9797@gmail.com>
2020-10-15 22:45:34 +0300

    Updates

---
+++ precache-manifest.abb4ed6e13018c3ec7d6882e100f4e64.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f7b41e767c3bed55cc373be3197bbec",
+    "url": "/UY-Scuty-Diary/index.html"
+  },
+  {
+    "revision": "a297f37b5f987305696a",
+    "url": "/UY-Scuty-Diary/static/css/2.3317f794.chunk.css"
+  },
+  {
+    "revision": "dabc44db5c1645fe3d5b",
+    "url": "/UY-Scuty-Diary/static/css/main.0fe8e629.chunk.css"
+  },
+  {
+    "revision": "a297f37b5f987305696a",
+    "url": "/UY-Scuty-Diary/static/js/2.0e4588c9.chunk.js"
+  },
+  {
+    "revision": "7b8402d31edc89b312c264c3810f08dd",
+    "url": "/UY-Scuty-Diary/static/js/2.0e4588c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabc44db5c1645fe3d5b",
+    "url": "/UY-Scuty-Diary/static/js/main.118ed28b.chunk.js"
+  },
+  {
+    "revision": "6f584ce868e76bc848ce",
+    "url": "/UY-Scuty-Diary/static/js/runtime-main.1ee36b4c.js"
+  },
+  {
+    "revision": "37573bf22bebab1c63f6be3a18c8cd70",
+    "url": "/UY-Scuty-Diary/static/media/UY Scuti.37573bf2.PNG"
+  },
+  {
+    "revision": "f8e7be9e7e394cffe3778d9d17d6096a",
+    "url": "/UY-Scuty-Diary/static/media/bg.f8e7be9e.jpg"
+  },
+  {
+    "revision": "13fd310d0035a8936b0f443af089d0d0",
+    "url": "/UY-Scuty-Diary/static/media/pen Icon.13fd310d.jpg"
+  },
+  {
+    "revision": "70ce08d26ed40b10164f8b82364cc8c2",
+    "url": "/UY-Scuty-Diary/static/media/save Icon.70ce08d2.jpg"
+  }
+]);
